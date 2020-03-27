4b283c0c79306ad35dc9a3f9ae0e8116d0bc3d20
gavinwsferguson <gavinwsferguson@gmail.com>
2020-03-27 18:37:50 +0000

    add user profile route & show page, update user model & register form

--- routes/index.js
+++ routes/index.js
@@ -19,10 +19,19 @@ router.get("/register", function (req, res) {
 
 // handle sign up logic
 router.post("/register", function (req, res) {
-    var newUser = new User({ username: req.body.username });
+    var newUser = new User(
+        {
+            username: req.body.username,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            avatar: req.body.avatar
+        });
+
     if (req.body.adminCode === "admincode123") {
         newUser.isAdmin = true;
     }
+
     User.register(newUser, req.body.password, function (err, user) {
         if (err) {
             // req.flash("error", err.message);
@@ -55,6 +64,18 @@ router.get("/logout", function (req, res) {
     req.logout();
     req.flash("success", "Logged you out!");
     res.redirect("/campgrounds");
+});
+
+// User profiles
+router.get("/users/:user_id", function (req, res) {
+    User.findById(req.params.user_id, function (err, foundUser) {
+        if (err) {
+            req.flash("error", "Something went wrong");
+            res.redirect("back");
+        } else {
+            res.render("users/show", { user: foundUser });
+        }
+    });
 });
 
 module.exports = router;
